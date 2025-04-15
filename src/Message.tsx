6621eeeee1b9ebf3b2becc3a5a843cad9bb5f99a
Yassin Lgaout <yassinlgaout@gmail.com>
2025-04-15 15:50:16 +0200

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+
+// PascalCasing : capitilise the first letter of every word in function components
+function Message() {
+    // JSX JavaScript XML for compiling react to JS 
+    return <h1>Hello world</h1>
+}
+
+export default Message;
